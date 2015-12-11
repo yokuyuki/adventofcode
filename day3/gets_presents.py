d4c15a98bc030f44550f6a18666249fab90bc528
Ed Chen <edychen@gmail.com>
2015-12-11 03:20:36 -0500

    Tabs -> spaces

--- day3/gets_presents.py
+++ day3/gets_presents.py
@@ -1,8 +1,8 @@
 with open('directions', 'r') as f:
-	x, y, history = 0, 0, set()
-	history.add((x,y))
-	for direction in f.read():
-		y = y if direction not in ('^', 'v') else y + 1 if direction == '^' else y - 1
-		x = x if direction not in ('<', '>') else x + 1 if direction == '>' else x - 1
-		history.add((x,y))
-	print(len(history))
+    x, y, history = 0, 0, set()
+    history.add((x,y))
+    for direction in f.read():
+        y = y if direction not in ('^', 'v') else y + 1 if direction == '^' else y - 1
+        x = x if direction not in ('<', '>') else x + 1 if direction == '>' else x - 1
+        history.add((x,y))
+    print(len(history))
