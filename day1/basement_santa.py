d77f332d82cebae3420bd41aa4971d18000823af
Ed Chen <edychen@gmail.com>
2015-12-11 00:31:39 -0500

    Updated solutions to use Python 3.4

--- day1/basement_santa.py
+++ day1/basement_santa.py
@@ -1,6 +1,8 @@
+import functools
+
 def floor_incrementer(floor, enumeration):
-    if floor == -1: print enumeration[0]
+    if floor == -1: print(enumeration[0])
     return floor + 1 if enumeration[1] == '(' else floor - 1
 
 with open('floor_instructions', 'r') as f:
-    reduce(lambda floor, enumeration: floor_incrementer(floor, enumeration), enumerate(f.read()), 0)
+    functools.reduce(lambda floor, enumeration: floor_incrementer(floor, enumeration), enumerate(f.read()), 0)
--- day1/what_floor.py
+++ day1/what_floor.py
@@ -1,2 +1,4 @@
+import functools
+
 with open('floor_instructions', 'r') as f:
-    print reduce(lambda floor, instruction: floor + 1 if instruction == '(' else floor - 1, f.read(), 0)
+    print(functools.reduce(lambda floor, instruction: floor + 1 if instruction == '(' else floor - 1, f.read(), 0))
