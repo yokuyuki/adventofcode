b7a51b65382b025db3078eac4b41aace1ebb1895
Ed Chen <edychen@gmail.com>
2015-12-11 18:41:35 -0500

    Solved second part of day 5

--- day5/nice_strings.py
+++ day5/nice_strings.py
@@ -1,8 +1,18 @@
-import functools
+import collections, functools
 
 def is_nice_string(string):
     vowels, letter_pairs, bad_pairs = functools.reduce(lambda state, letter_pair: (state[0] + 1 if letter_pair[0] in 'aeiou' else state[0], state[1] + 1 if letter_pair[0] == letter_pair[1] else state[1], state[2] + 1 if letter_pair[1] + letter_pair[0] in ('ab', 'cd', 'pq', 'xy') else state[2]), zip(string, ' ' + string), (0, 0, 0))
     return vowels >= 3 and letter_pairs >= 1 and bad_pairs == 0
 
+def is_super_nice_string(string):
+    non_overlapping_pair, three_letter_palindrome, letter_pairs = False, False, collections.defaultdict(list)
+    for index, letter, previous_letter, previous_previous_letter in zip(range(len(string)), string, ' ' + string, '  ' + string):
+        non_overlapping_pair = True if previous_previous_letter == letter else non_overlapping_pair
+        three_letter_palindrome = True if previous_letter + letter in letter_pairs and functools.reduce(lambda palindrome_exists, old_index: palindrome_exists or index - old_index > 1, letter_pairs[previous_letter + letter], False) else three_letter_palindrome
+        if non_overlapping_pair and three_letter_palindrome: return True
+        letter_pairs[previous_letter + letter].append(index)
+
 with open('strings', 'r') as f:
-    print(functools.reduce(lambda count, string: count + 1 if is_nice_string(string) else count, f, 0))
+    strings = [line for line in f]
+    print(functools.reduce(lambda count, string: count + 1 if is_nice_string(string) else count, strings, 0))
+    print(functools.reduce(lambda count, string: count + 1 if is_super_nice_string(string) else count, strings, 0))
