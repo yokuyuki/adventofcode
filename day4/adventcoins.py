4272cebf25c05b893cb0d781253e709917a45bed
Ed Chen <edychen@gmail.com>
2015-12-12 02:59:48 -0500

    Shaved off one line using lambdas on day 4

--- day4/adventcoins.py
+++ day4/adventcoins.py
@@ -1,15 +1,14 @@
-import hashlib
+from hashlib import md5
 
-def mine(leading_zeroes = 5):
-    third_byte_less_than = 1 if leading_zeroes == 6 else 16
-    base, i = hashlib.md5('yzbqklnj'.encode('utf-8')), 0
+def mine(good_md5sum):
+    base, i = md5('yzbqklnj'.encode('utf-8')), 0
     while True:
         m, i = base.copy(), i+1
         m.update(str(i).encode('utf-8'))
         md5sum = m.digest()
-        if md5sum[0] == md5sum[1] == 0 and md5sum[2] < third_byte_less_than:
+        if good_md5sum(md5sum):
             print(i)
             break
 
-mine(5)
-mine(6)
+mine(lambda md5sum: True if md5sum[0] == md5sum[1] == (md5sum[2] & 0xf0) == 0 else False)
+mine(lambda md5sum: True if md5sum[0] == md5sum[1] == md5sum[2] == 0 else False)
