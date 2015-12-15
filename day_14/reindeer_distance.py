7b1eb868030da8aa63f77399870b3954cd9ede79
Ed Chen <edychen@gmail.com>
2015-12-14 23:44:10 -0500

    Solved first part of day 14

---
+++ day_14/reindeer_distance.py
@@ -0,0 +1,15 @@
+import re
+
+def race(speed, endurance, recovery_time, time):
+    iterations, partial_iteration_time = time // (endurance + recovery_time), time % (endurance + recovery_time)
+    travel_time = iterations * endurance + partial_iteration_time if partial_iteration_time < endurance else iterations * endurance + endurance
+    return speed * travel_time
+
+with open('reindeer_descriptions', 'r') as f:
+    furthest = 0
+    for description in f:
+        m = re.search(r"^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.", description)
+        name, speed, endurance, recovery_time = m.group(1), int(m.group(2)), int(m.group(3)), int(m.group(4))
+        distance = race(speed, endurance, recovery_time, 2503)
+        furthest = distance if distance > furthest else furthest
+    print(furthest)
