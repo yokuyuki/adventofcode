78f32dfd8c0c6c5ec77be2b438a1901f66e52981
Ed Chen <edychen@gmail.com>
2015-12-15 02:25:29 -0500

    Single line functions -> lambda

--- day_02/order_ribbon.py
+++ day_02/order_ribbon.py
@@ -1,8 +1,6 @@
 import functools, operator
 
-def wrap(dimensions):
-    return 2*(dimensions[0]+dimensions[1]) + functools.reduce(operator.mul, dimensions)
-
 with open('gifts', 'r') as f:
     sizes = [sorted(map(int, size.rstrip().split('x'))) for size in f]
+    wrap = lambda dimensions: 2*(dimensions[0]+dimensions[1]) + functools.reduce(operator.mul, dimensions)
     print(functools.reduce(lambda sum, size: sum + wrap(size), sizes, 0))
