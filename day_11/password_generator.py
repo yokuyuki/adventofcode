2f3ade3d5d527b4b3717b40a2b966709e3fe3f36
Ed Chen <edychen@gmail.com>
2015-12-14 23:13:28 -0500

    Tabs -> spaces

--- day_11/password_generator.py
+++ day_11/password_generator.py
@@ -1,17 +1,17 @@
 from functools import reduce
 
 def is_valid_password(password):
-	distinct_pairs = set([letter*2 for letter, previous_letter in zip(password, ' ' + password) if letter == previous_letter])
-	three_consecutive_letters, no_bad_letters = reduce(lambda state, letter_triplet: (True if ord(letter_triplet[2]) + 2 == ord(letter_triplet[1]) + 1 == ord(letter_triplet[0]) else state[0], False if letter_triplet[0] in 'oil' else state[1]), zip(password, ' ' + password, '  '+ password), (False, True))
-	return len(distinct_pairs) >= 2 and three_consecutive_letters and no_bad_letters
+    distinct_pairs = set([letter*2 for letter, previous_letter in zip(password, ' ' + password) if letter == previous_letter])
+    three_consecutive_letters, no_bad_letters = reduce(lambda state, letter_triplet: (True if ord(letter_triplet[2]) + 2 == ord(letter_triplet[1]) + 1 == ord(letter_triplet[0]) else state[0], False if letter_triplet[0] in 'oil' else state[1]), zip(password, ' ' + password, '  '+ password), (False, True))
+    return len(distinct_pairs) >= 2 and three_consecutive_letters and no_bad_letters
 
 def next_password(password):
-	integerize = lambda password: reduce(lambda num, enumeration: num + (ord(enumeration[1]) - ord('a')) * 26**enumeration[0],enumerate(password[::-1]), 0)
-	alphabetize = lambda integer: '' if integer == 0 else alphabetize(integer // 26) + chr(integer % 26 + ord('a'))
-	new_password = alphabetize(integerize(password) + 1)
-	while not is_valid_password(new_password):
-		new_password = alphabetize(integerize(new_password) + 1)
-	print(new_password)
-	return new_password
+    integerize = lambda password: reduce(lambda num, enumeration: num + (ord(enumeration[1]) - ord('a')) * 26**enumeration[0],enumerate(password[::-1]), 0)
+    alphabetize = lambda integer: '' if integer == 0 else alphabetize(integer // 26) + chr(integer % 26 + ord('a'))
+    new_password = alphabetize(integerize(password) + 1)
+    while not is_valid_password(new_password):
+        new_password = alphabetize(integerize(new_password) + 1)
+    print(new_password)
+    return new_password
 
 next_password(next_password('cqjxjnds'))
--- day_12/all_numbers.py
+++ day_12/all_numbers.py
@@ -1,5 +1,5 @@
 import functools, re
 
 with open('document', 'r') as f:
-	numbers = re.findall(r"(?<!\")(-?\d+)(?!\")", f.read())
-	print(functools.reduce(lambda sum, number: sum + int(number), numbers, 0))
+    numbers = re.findall(r"(?<!\")(-?\d+)(?!\")", f.read())
+    print(functools.reduce(lambda sum, number: sum + int(number), numbers, 0))
