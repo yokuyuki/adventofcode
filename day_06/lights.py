47df2c8db35be9646bfae7f14c525dd5e797fc7a
Ed Chen <edychen@gmail.com>
2015-12-15 02:36:41 -0500

    Use itertools.product to save 1 line in day 6

--- day_06/lights.py
+++ day_06/lights.py
@@ -1,4 +1,4 @@
-import collections, functools, operator, re
+import collections, functools, itertools, operator, re
 
 def light_pattern(instruction_translation):
     with open('lighting_configuration', 'r') as f:
@@ -7,9 +7,8 @@ def light_pattern(instruction_translation):
             m = re.search(r"([\w ]+) (\d+),(\d+) through (\d+),(\d+)", step)
             instruction, x1, y1, x2, y2 = m.group(1), int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5))
             fn = instruction_translation(instruction)
-            for x in range(x1, x2 + 1):
-                for y in range(y1, y2 + 1):
-                    lights[x][y] = fn(lights[x][y])
+            for x, y in itertools.product(range(x1, x2 + 1), range(y1, y2 + 1)):
+                lights[x][y] = fn(lights[x][y])
         print(functools.reduce(lambda sum, col: sum + functools.reduce(operator.add, col.values(), 0), lights.values(),0))
 
 light_pattern(lambda instruction: (lambda light: 1) if instruction == 'turn on' else (lambda light: 0) if instruction == 'turn off' else (lambda light: (light + 1) % 2))
