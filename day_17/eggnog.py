8769e5b9d3207c5d5289b2eebf1dee900bb1a5b7
Ed Chen <edychen@gmail.com>
2015-12-17 02:10:59 -0500

    Solved second part of day 17

--- day_17/eggnog.py
+++ day_17/eggnog.py
@@ -1,5 +1,7 @@
 with open('containers', 'r') as f:
-	containers, combos = sorted([int(x) for x in f]), set()
-	find_combos = lambda remaining_eggnog, used, combos=combos, containers=containers: combos.add(used) if not remaining_eggnog else any(find_combos(remaining_eggnog - container, used | 1 << i) for i, container in enumerate(containers) if container <= remaining_eggnog and not used & 1 << i)
-	find_combos(150, 0)
-	print(len(combos))
+    containers, combos = sorted([int(x) for x in f]), set()
+    find_combos = lambda remaining_eggnog, used, combos=combos, containers=containers: combos.add(used) if not remaining_eggnog else any(find_combos(remaining_eggnog - container, used | 1 << i) for i, container in enumerate(containers) if container <= remaining_eggnog and not used & 1 << i)
+    find_combos(150, 0)
+    print(len(combos))
+    used_containers = [bin(combo).count('1') for combo in combos]
+    print(used_containers.count(min(used_containers)))
