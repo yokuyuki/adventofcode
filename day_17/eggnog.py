f92c722edf4535065dbd5f2b8fcc9e0195ea86f8
Ed Chen <edychen@gmail.com>
2015-12-17 01:48:47 -0500

    Save 5 lines by replacing method definition with lambda
    * Nasty hack of using any to avoid using any memory storing the result in the for loop

--- day_17/eggnog.py
+++ day_17/eggnog.py
@@ -1,11 +1,5 @@
-def find_combos(combos, containers, remaining_eggnog=150, used=0):
-	if remaining_eggnog == 0:
-		combos.add(used)
-	else:
-		for i, container in ((i, container) for i, container in enumerate(containers) if container <= remaining_eggnog and not used & 1 << i):
-			find_combos(combos, containers, remaining_eggnog - container, used | 1 << i)
-
 with open('containers', 'r') as f:
 	containers, combos = sorted([int(x) for x in f]), set()
-	find_combos(combos, containers)
+	find_combos = lambda remaining_eggnog, used, combos=combos, containers=containers: combos.add(used) if not remaining_eggnog else any(find_combos(remaining_eggnog - container, used | 1 << i) for i, container in enumerate(containers) if container <= remaining_eggnog and not used & 1 << i)
+	find_combos(150, 0)
 	print(len(combos))
