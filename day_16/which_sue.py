8551e95ecf8d4347a0a0d2e39a60842683f77711
Ed Chen <edychen@gmail.com>
2015-12-17 00:02:53 -0500

    Solved second part of day 16

--- day_16/which_sue.py
+++ day_16/which_sue.py
@@ -1,10 +1,13 @@
-import re
+import functools, re
 
-with open('sues', 'r') as f:
-    hint = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0, 'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
-    for sue in f:
-        m = re.search(r"^Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)", sue)
-        sue_num, item1, num1, item2, num2, item3, num3 = m.group(1), m.group(2), int(m.group(3)), m.group(4), int(m.group(5)), m.group(6), int(m.group(7))
-        if item1 in hint and hint[item1] == num1 and item2 in hint and hint[item2] == num2 and item3 in hint and hint[item3] == num3:
-            print(sue_num)
-            break
+def find_sue(comparison):
+    with open('sues', 'r') as f:
+        for sue in f:
+            m, traits = re.search(r"^Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)", sue), {}
+            sue_num, traits[m.group(2)], traits[m.group(4)], traits[m.group(6)] = m.group(1), int(m.group(3)), int(m.group(5)), int(m.group(7))
+            if comparison(traits):
+                return sue_num
+
+hint = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0, 'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
+print(find_sue(lambda traits, hint=hint: functools.reduce(lambda correct_sue, trait: correct_sue and trait[0] in hint and hint[trait[0]] == trait[1], traits.items(), True)))
+print(find_sue(lambda traits, hint=hint: functools.reduce(lambda correct_sue, trait: correct_sue and trait[0] in hint and (hint[trait[0]] < trait[1] if trait[0] in ('cats', 'trees') else hint[trait[0]] > trait[1] if trait[0] in ('pomeranians', 'goldfish') else hint[trait[0]] == trait[1]), traits.items(), True)))
