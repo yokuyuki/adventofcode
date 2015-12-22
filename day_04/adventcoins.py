f2425a202804c4295ff5a3d9247ad9365fd13210
Ed Chen <edychen@gmail.com>
2015-12-21 23:02:14 -0800

    Use itertools.count for infinite iterator

--- day_04/adventcoins.py
+++ day_04/adventcoins.py
@@ -1,12 +1,11 @@
-from hashlib import md5
+import hashlib, itertools
 
 def mine(good_md5sum):
-    base, i = md5('yzbqklnj'.encode('utf-8')), 0
-    while True:
-        m, i = base.copy(), i+1
+    base = hashlib.md5('yzbqklnj'.encode('utf-8'))
+    for i in itertools.count():
+        m = base.copy()
         m.update(str(i).encode('utf-8'))
-        if good_md5sum(m.digest()):
-            return i
+        if good_md5sum(m.digest()): return i
 
 print(mine(lambda md5sum: True if md5sum[0] == md5sum[1] == (md5sum[2] & 0xf0) == 0 else False))
 print(mine(lambda md5sum: True if md5sum[0] == md5sum[1] == md5sum[2] == 0 else False))
