70b01cfce93738908f5e3d26846b457679033901
Ed Chen <edychen@gmail.com>
2016-12-14 01:13:45 -0500

    Solved first part of day 14

---
+++ 2016/day_14/OneTimePad.java
@@ -0,0 +1,72 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+public class OneTimePad {
+
+    private String salt;
+    private MessageDigest md5;
+    private ArrayList<String> hashes;
+
+    public OneTimePad(String salt) {
+        this.salt = salt;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int[] generateKeys(int numOfKeys) {
+        hashes = new ArrayList<>();
+        return IntStream.iterate(0, i -> i + 1)
+                .peek(i -> {
+                    if (i == hashes.size()) {
+                        generate1000Hashes(i);
+                    }
+                })
+                .filter(this::isValidKey)
+                .limit(numOfKeys)
+                .toArray();
+    }
+
+    private boolean isValidKey(int index) {
+        String hash = hashes.get(index);
+        Optional<Character> tripleChar = IntStream.rangeClosed(0, hash.length() - 3)
+                .filter(i -> hash.charAt(i) == hash.charAt(i+1) && hash.charAt(i) == hash.charAt(i+2))
+                .mapToObj(hash::charAt)
+                .findFirst();
+
+        return tripleChar.isPresent() && IntStream.rangeClosed(index + 1, index + 1000)
+                .peek(i -> {
+                    if (i == hashes.size()) {
+                        generate1000Hashes(i);
+                    }
+                })
+                .anyMatch(i -> isFiveOfAKind(i, tripleChar.get()));
+    }
+
+    private boolean isFiveOfAKind(int index, char kind) {
+        String hash = hashes.get(index);
+        return IntStream.rangeClosed(0, hash.length() - 5)
+                .anyMatch(i -> IntStream.range(i, i + 5)
+                        .allMatch(j -> hash.charAt(j) == kind));
+    }
+
+    private void generate1000Hashes(int index) {
+        IntStream.range(index, index + 1000)
+                .mapToObj(i -> {
+                    md5.update((salt + i).getBytes());
+                    return String.format("%032x", new BigInteger(1, md5.digest()));
+                }).forEachOrdered(hashes::add);
+    }
+
+    public static void main(String args[]) {
+        OneTimePad otp = new OneTimePad("jlmsuwbz");
+        int[] keys = otp.generateKeys(64);
+        System.out.println("Part 1: " + keys[63]);
+    }
+}
