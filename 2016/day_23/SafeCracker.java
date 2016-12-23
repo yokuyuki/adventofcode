4331588c34b16d46abb8d6af3b5466188e0ad4b6
Ed Chen <edychen@gmail.com>
2016-12-22 23:20:29 -0800

    Make similar optimization on day 23 as day 12

--- 2016/day_23/SafeCracker.java
+++ 2016/day_23/SafeCracker.java
@@ -4,25 +4,87 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class SafeCracker {
 
-    private ArrayList<String> instructions;
+    private static class Instruction {
+        enum TYPE {
+            CPY, INC, DEC, JNZ, TGL
+        }
+
+        private TYPE type;
+        private String param1, param2;
+
+        Instruction(String instruction) {
+            String[] parsed = instruction.split(" ");
+            switch (parsed[0]) {
+                case "cpy":
+                    this.type = TYPE.CPY;
+                    break;
+                case "inc":
+                    this.type = TYPE.INC;
+                    break;
+                case "dec":
+                    this.type = TYPE.DEC;
+                    break;
+                case "jnz":
+                    this.type = TYPE.JNZ;
+                    break;
+                case "tgl":
+                    this.type = TYPE.TGL;
+                    break;
+            }
+            this.param1 = parsed[1];
+            this.param2 = parsed.length < 3 ? null : parsed[2];
+        }
+
+        TYPE getType() {
+            return type;
+        }
+
+        String getParam1() {
+            return param1;
+        }
+
+        String getParam2() {
+            return param2;
+        }
+
+        void toggle() {
+            switch (type) {
+                case CPY:
+                    type = TYPE.JNZ;
+                    break;
+                case JNZ:
+                    type = TYPE.CPY;
+                    break;
+                case INC:
+                    type = TYPE.DEC;
+                    break;
+                case DEC:
+                case TGL:
+                    type = TYPE.INC;
+                    break;
+            }
+        }
+    }
+
+    private ArrayList<Instruction> instructions;
     private Map<Character, Integer> registers;
 
     public SafeCracker(Map<Character, Integer> initialValues) {
         try {
             registers = initialValues;
-            instructions = Files.lines(Paths.get("day_23/instructions")).collect(Collectors.toCollection(ArrayList::new));
+            instructions = Files.lines(Paths.get("day_23/instructions"))
+                    .map(Instruction::new)
+                    .collect(Collectors.toCollection(ArrayList::new));
             executeInstructions();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public int getRegister(char register) {
-        return registers.get(register);
     }
 
     public int getValue(String value) {
@@ -31,63 +93,57 @@ public class SafeCracker {
     }
 
     private void executeInstructions() {
-        int index = 0;
-        while (index < instructions.size()) {
-            String[] parsedInstruction = instructions.get(index).split(" ");
-            switch (parsedInstruction[0]) {
-                case "cpy":
-                    char dest = parsedInstruction[2].charAt(0);
-                    char src = parsedInstruction[1].charAt(0);
-                    if (Character.isLetter(src)) {
-                        registers.put(dest, registers.get(src));
-                    } else if (Character.isLetter(dest)) {
-                        registers.put(dest, Integer.parseInt(parsedInstruction[1]));
-                    }
-                    break;
-                case "inc":
-                    dest = parsedInstruction[1].charAt(0);
-                    registers.put(dest, registers.get(dest) + 1);
-                    break;
-                case "dec":
-                    dest = parsedInstruction[1].charAt(0);
-                    registers.put(dest, registers.get(dest) - 1);
-                    break;
-                case "jnz":
-                    int value = getValue(parsedInstruction[1]);
-                    if (value != 0) {
-                        index += getValue(parsedInstruction[2]);
-                        continue;
-                    }
-                    break;
-                case "tgl":
-                    toggleInstruction(index + getValue(parsedInstruction[1]));
-                    break;
-            }
-            index++;
+        AtomicInteger index = new AtomicInteger();
+        IntStream.iterate(0, i -> i + 1)
+                .allMatch(i -> index.updateAndGet(this::executeInstruction) < instructions.size());
+    }
+
+    private int executeInstruction(int index) {
+        Instruction instruction = instructions.get(index);
+        switch (instruction.getType()) {
+            case CPY:
+                copy(instruction);
+                break;
+            case INC:
+                increment(instruction);
+                break;
+            case DEC:
+                decrement(instruction);
+                break;
+            case JNZ:
+                return index + jumpIfNotZero(instruction);
+            case TGL:
+                toggle(instruction, index);
+                break;
+        }
+        return index + 1;
+    }
+
+    private void copy(Instruction instruction) {
+        char dest = instruction.getParam2().charAt(0);
+        if (Character.isLetter(dest)) {
+            registers.put(dest, getValue(instruction.getParam1()));
         }
     }
 
-    private void toggleInstruction(int index) {
-        if (index < instructions.size()) {
-            String instruction = instructions.get(index);
-            String[] parsedInstruction = instruction.split(" ");
-            String replacement;
-            switch (parsedInstruction[0]) {
-                case "cpy":
-                    replacement = "jnz";
-                    break;
-                case "jnz":
-                    replacement = "cpy";
-                    break;
-                case "inc":
-                    replacement = "dec";
-                    break;
-                default:
-                    replacement = "inc";
-                    break;
-            }
-            instruction = instruction.replace(parsedInstruction[0], replacement);
-            instructions.set(index, instruction);
+    private void increment(Instruction instruction) {
+        char dest = instruction.getParam1().charAt(0);
+        registers.put(dest, registers.get(dest) + 1);
+    }
+
+    private void decrement(Instruction instruction) {
+        char dest = instruction.getParam1().charAt(0);
+        registers.put(dest, registers.get(dest) - 1);
+    }
+
+    private int jumpIfNotZero(Instruction instruction) {
+        return getValue(instruction.getParam1()) != 0 ? getValue(instruction.getParam2()) : 1;
+    }
+
+    private void toggle(Instruction instruction, int currentIndex) {
+        int toggleIndex = currentIndex + getValue(instruction.getParam1());
+        if (toggleIndex < instructions.size()) {
+            instructions.get(toggleIndex).toggle();
         }
     }
 
@@ -98,6 +154,6 @@ public class SafeCracker {
             this.put('c', 0);
             this.put('d', 0);
         }});
-        System.out.println("Part 1: " + sc.getRegister('a'));
+        System.out.println("Part 1: " + sc.getValue("a"));
     }
 }
