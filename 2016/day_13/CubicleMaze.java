d65452847daeb0b3c58ea60b235250f74a81bc5d
Ed Chen <edychen@gmail.com>
2016-12-24 12:03:01 -0800

    Use BFS to solve day 13 faster

--- 2016/day_13/CubicleMaze.java
+++ 2016/day_13/CubicleMaze.java
@@ -1,9 +1,19 @@
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Optional;
+import java.util.LinkedList;
 import java.util.stream.IntStream;
 
 public class CubicleMaze {
+
+    private static class CubicleState {
+        public int y, x;
+        public int steps;
+
+        CubicleState(int y, int x, int steps) {
+            this.y = y;
+            this.x = x;
+            this.steps = steps;
+        }
+    }
 
     private int seed;
 
@@ -21,53 +31,40 @@ public class CubicleMaze {
                 });
     }
 
-    public int minWalk(int x, int y) {
-        return walk(1, 1, x, y, new HashSet<>());
-    }
-
-    private int walk(int posX, int posY, int destX, int destY, HashSet<String> visited) {
-        String position = posX + "," + posY;
-        if (posX < 0 || posX < 0 || isWall(posX, posY) || visited.contains(position)) {
-            return Integer.MAX_VALUE;
-        }
-
-        if (posX == destX && posY == destY) {
-            return 0;
-        }
-
-        visited.add(position);
-        ArrayList<Integer> steps = new ArrayList<>(4);
-        steps.add(walk(posX + 1, posY, destX, destY, visited));
-        steps.add(walk(posX - 1, posY, destX, destY, visited));
-        steps.add(walk(posX, posY + 1, destX, destY, visited));
-        steps.add(walk(posX, posY - 1, destX, destY, visited));
-        Optional<Integer> minSteps = steps.parallelStream()
-                .filter(i -> i != Integer.MAX_VALUE)
-                .min(Integer::compareTo);
-        visited.remove(position);
-
-        return minSteps.map(i -> i + 1).orElse(Integer.MAX_VALUE);
-    }
-
-    public int maxDistinctWalk(int steps) {
+    public int getMinWalk(int x, int y) {
+        LinkedList<CubicleState> queue = new LinkedList<>();
+        queue.addLast(new CubicleState(1, 1, 0));
         HashSet<String> visited = new HashSet<>();
-        distinctWalk(1, 1, steps, visited, new HashSet<>());
-        return visited.size();
+        return IntStream.iterate(0, i -> i+1)
+                .mapToObj(i -> queue.removeFirst())
+                .peek(state -> nextWalk(state, queue, visited))
+                .filter(state -> state.y == y && state.x == x)
+                .mapToInt(state -> state.steps)
+                .findFirst().getAsInt();
     }
 
-    private void distinctWalk(int x, int y, int steps, HashSet<String> visited, HashSet<String> route) {
-        String position = x + "," + y;
-        if (x < 0 || y < 0 || isWall(x, y) || route.contains(position) || steps < 0) {
-            return;
+    public int getDistinctWalk(int steps) {
+        LinkedList<CubicleState> queue = new LinkedList<>();
+        queue.addLast(new CubicleState(1, 1, 0));
+        HashSet<String> visited = new HashSet<>();
+        while (true) {
+            CubicleState state = queue.removeFirst();
+            if (state.steps > steps) {
+                return visited.size();
+            }
+            nextWalk(state, queue, visited);
         }
+    }
 
-        route.add(position);
-        visited.add(position);
-        distinctWalk(x + 1, y, steps - 1, visited, route);
-        distinctWalk(x - 1, y, steps - 1, visited, route);
-        distinctWalk(x, y + 1, steps - 1, visited, route);
-        distinctWalk(x, y - 1, steps - 1, visited, route);
-        route.remove(position);
+    private void nextWalk(CubicleState state, LinkedList<CubicleState> queue, HashSet<String> visited) {
+        String position = state.y + "," + state.x;
+        if (state.y >= 0 && state.x >= 0 && !isWall(state.x, state.y) && !visited.contains(position)) {
+            visited.add(position);
+            queue.addLast(new CubicleState(state.y-1, state.x, state.steps+1));
+            queue.addLast(new CubicleState(state.y+1, state.x, state.steps+1));
+            queue.addLast(new CubicleState(state.y, state.x-1, state.steps+1));
+            queue.addLast(new CubicleState(state.y, state.x+1, state.steps+1));
+        }
     }
 
     private boolean isWall(int x, int y) {
@@ -81,8 +78,7 @@ public class CubicleMaze {
 
     public static void main(String args[]) {
         CubicleMaze maze = new CubicleMaze(1350);
-        System.out.println("Part 1: " + maze.minWalk(31, 39));
-        System.out.println("Part 2: " + maze.maxDistinctWalk(50));
+        System.out.println("Part 1: " + maze.getMinWalk(31, 39));
+        System.out.println("Part 2: " + maze.getDistinctWalk(50));
     }
-
 }
